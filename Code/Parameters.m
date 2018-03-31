b210993b3ca8f513db70c4ba62dc981610bdcc80
Omer Yuval <omer1yuval1@gmail.com>
2018-03-31 17:55:37 +0100

    commented a line in the parameters file used for development.

--- Code/Parameters.m
+++ Code/Parameters.m
@@ -3,7 +3,7 @@ function P = Parameters()
 	P = struct();
 	
 	P(1).FileName = '';
-	P.FileName = 'D:\Work\Leeds\Movies\Tierpsy\20180328\Results\Basler acA4024-29um (22602116)_20180328_105333684_features.hdf5'; % Single worm sample file.
+	% P.FileName = 'D:\Work\Leeds\Movies\Tierpsy\20180328\Results\Basler acA4024-29um (22602116)_20180328_105333684_features.hdf5'; % Single worm sample file.
 	% P.FileName = 'D:\Work\Leeds\Movies\Tierpsy\20180322_Lan_CB177\Results\CB177 20uL OP50\20uL_OP50_1_20180322_151132160_features.hdf5'; % Lan's sample file.
 	
 	P.Min_Frames_Number = 5;
